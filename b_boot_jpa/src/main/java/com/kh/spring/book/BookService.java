7501d1041368de60ec65200dd836f1f453293445
noob000001 <kcdpads3@naver.com>
2021-11-02 15:28:31 +0900

    [이규원] queryDSL

---
+++ b_boot_jpa/src/main/java/com/kh/spring/book/BookService.java
@@ -0,0 +1,22 @@
+package com.kh.spring.book;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+
+public class BookService {
+
+	private final BookRepository bookRepository;
+
+	@Transactional
+	public void deleteByTitle(String title) {
+		bookRepository.deleteByTitle(title);
+	}
+	
+}
--- b_boot_jpa/src/test/java/com/kh/spring/springdata/SpringDataRepository.java
+++ b_boot_jpa/src/test/java/com/kh/spring/springdata/SpringDataRepository.java
@@ -1,9 +1,23 @@
 package com.kh.spring.springdata;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kh.spring.book.Book;
 
 public interface SpringDataRepository extends JpaRepository<Book, Long>{
 
+	//도서명 또는 작가로 도서 검색
+	List<Book> findByTitleOrAuthor(String title, String author);
+	
+	//카테고리가 문학이고, 도서 재고가 3권 이상이면서 도서명이 디로 시작하는 도서를 조회
+	List<Book> findByCategoryAndBookAmtGreaterThanEqualAndTitleStartingWith(String category, int bookAmt, String title);
+	
+	int countByBookAmtGreaterThanEqual(int bookAmt);
+	
+	boolean existsByBookAmtGreaterThanEqual(int bookAmt);
+	
+	void deleteByTitle(String title);
+	
 }
